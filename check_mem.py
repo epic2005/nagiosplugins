c5510a025de049adbd88dd03ebfe9fa57e1c3926
lipengyue <lipengyue@gmail.com>
2013-07-25 14:43:27 +0800

    Update check_mem.py

--- check_mem.py
+++ check_mem.py
@@ -1,23 +1,31 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 
-from optparse import OptionParser
+"""
+Mod By:
+    epic2005 13:17 2013/7/25
+modfication:
+    commit scaleUnit func.
+"""
+
 import sys
+from optparse import OptionParser
+import string
 
 OK=0
 WARNING=1
-CRICITCAL=2
+CRITICAL=2
 UNKNOWN=3
+
+unit = {'t':2**40,'g':2**30,'m':2**20,'k':2**10,'b':1}
 
 def opt():
     parser = OptionParser(usage="usage: %prog -w WARNING -c CRITICAL")
-    parser.add_option("-c", default='100M', action="store", type="string", dest="critical")
-    parser.add_option("-w", default='200M', action="store", type="string", dest="warning")
+    parser.add_option("-c", default="100M", action="store", type="string", dest="critical")
+    parser.add_option("-w", default="500M", action="store", type="string", dest="warning")
     return parser.parse_args()
 
 
 def convertUnit(s):
-    unit = {'g':2**30,'m':2**20,'k':2**10,'t':2**40,'b':1}
     s = s.lower()
     lastchar = s[-1]
     num = int(s[:-1])
@@ -26,31 +34,42 @@ def convertUnit(s):
     else:
         return int(s)
 
+def scaleUnit(d):
+    for k,v in unit.items():
+        num = d / v
+        if (0 < num < 2**10):
+            return num,k
 
 def getFreeMemory():
-    with open('/proc/meminfo', 'r') as fd:
+    with open('/proc/meminfo','r') as fd:
         for line in fd.readlines():
             if line.startswith('MemFree'):
-                 k, v, u = line.split()
-                 return int(v)*1024
-                       
+                k, v, u =  line.split()
+                return int(v)*1024
+
 def main():
     opts, args = opt()
+
     w = convertUnit(opts.warning)
     c = convertUnit(opts.critical)
-    free_mem = getFreeMemory()
+    w,u =  scaleUnit(w)
+    c,u =  scaleUnit(c)
+
+    free_mem =  getFreeMemory()
+    free_mem,u = scaleUnit(free_mem)
+
     if w >= free_mem > c:
-        print "WARNING, free:%s MB" % free_mem
+        print "WARNING, free: %s %s" % (free_mem,u)
         sys.exit(WARNING)
     elif free_mem <= c:
-        print "CRICITCAL, free:%s MB" % free_mem
-        sys.exit(CRICITCAL)
-    elif free_mem > w:
-        print "OK, free:%s MB" % free_mem
+        print "CRITICAL, free: %s %s" % (free_mem,u)
+        sys.exit(CRITICAL)
+    elif free_mem > w :
+        print "OK, free:%s %s" % (free_mem,u)
         sys.exit(OK)
     else:
-        print "UNKNOWN, free:%s MB" % free_mem
+        print "UNKNONW, free:%s %s" % (free_mem,u)
         sys.exit(UNKNOWN)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
